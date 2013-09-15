acff4ce38b85a7eba88a73775da6f5ea0e53211a
/Top <loki.top11@gmail.com>
2013-09-15 09:05:38 +0700

    Add order list

--- gen/com/example/pomtask/R.java
+++ gen/com/example/pomtask/R.java
@@ -173,9 +173,9 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int textfield_focused_holo_light=0x7f020079;
     }
     public static final class id {
-        public static final int action_search=0x7f08000a;
-        public static final int action_settings=0x7f08000b;
-        public static final int content_new=0x7f080009;
+        public static final int action_search=0x7f08000b;
+        public static final int action_settings=0x7f08000c;
+        public static final int content_new=0x7f08000a;
         public static final int pager=0x7f080000;
         public static final int priority=0x7f080002;
         public static final int radio_none=0x7f080003;
@@ -184,6 +184,7 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int radio_p3=0x7f080006;
         public static final int radio_star=0x7f080007;
         public static final int select_list=0x7f080008;
+        public static final int select_order=0x7f080009;
         public static final int taskname=0x7f080001;
     }
     public static final class layout {
@@ -201,6 +202,7 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int app_name=0x7f050000;
         public static final int content_new=0x7f050003;
         public static final int label_list=0x7f050005;
+        public static final int label_order=0x7f05000b;
         public static final int prior_none=0x7f05000a;
         public static final int prior_one=0x7f050007;
         public static final int prior_star=0x7f050006;
