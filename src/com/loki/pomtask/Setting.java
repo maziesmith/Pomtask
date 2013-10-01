463eeb229be011e2035d8426432fa0c07e760c4c
prideloki <loki.top11@gmail.com>
2013-10-01 16:09:30 +0700

    setting3

--- src/com/loki/pomtask/Setting.java
+++ src/com/loki/pomtask/Setting.java
@@ -1,28 +1,26 @@
 package com.loki.pomtask;
 
-import com.example.pomtask.R;
 
-import android.app.ActionBar;
-import android.app.ActionBar.Tab;
-import android.app.FragmentTransaction;
-import android.content.Intent;
-import android.os.Build;
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import android.view.Menu;
-import android.preference.*;
-
-public class Setting  {
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
-}
+public class Setting extends ListActivity {
+	  public void onCreate(Bundle icicle) {
+		    super.onCreate(icicle);
+		    String[] values = new String[] { "Statistics","Set a Timer","About" };
+		    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+		        android.R.layout.simple_list_item_1, values);
+		    setListAdapter(adapter);
+		  }
+
+		  @Override
+		  protected void onListItemClick(ListView l, View v, int position, long id) {
+		    String item = (String) getListAdapter().getItem(position);
+		    Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
+		  }
+		} 
