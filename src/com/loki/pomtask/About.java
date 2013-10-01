ca96847612ca1fca70ea66722ef964eb98ad5bbe
prideloki <loki.top11@gmail.com>
2013-10-01 16:45:54 +0700

    setting+about

---
+++ src/com/loki/pomtask/About.java
@@ -0,0 +1,12 @@
+package com.loki.pomtask;
+
+import com.example.pomtask.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity {
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		setContentView(R.layout.about);
+	}
+}
--- src/com/loki/pomtask/Setting.java
+++ src/com/loki/pomtask/Setting.java
@@ -1,6 +1,5 @@
 package com.loki.pomtask;
 
-
 import com.example.pomtask.R;
 
 import android.app.ListActivity;
@@ -12,44 +11,45 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-
 public class Setting extends ListActivity {
-	  public void onCreate(Bundle icicle) {
-		    super.onCreate(icicle);
-		    String[] values = new String[] { "Statistics","Set a Timer","About" };
-		    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-		        android.R.layout.simple_list_item_1, values);
-		    setListAdapter(adapter);
-		  }
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		String[] values = new String[] { "Statistics", "Set a Timer", "About" };
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, values);
+		setListAdapter(adapter);
+	}
 
-		  @Override
-		  protected void onListItemClick(ListView l, View v, int position, long id) {
-		    String item = (String) getListAdapter().getItem(position);
-		    Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
-		  }
-		} 
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		String item = (String) getListAdapter().getItem(position);
 
-public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle presses on the action bar items
-    switch (item.getItemId()) {
-        case R.id.action_search:
-            //openSearch();
-            return true;
-        case R.id.action_settings:
-            openSettings();
-            return true;
-        case R.id.content_new:
-        	openNewContent();
-        	return true;
-        default:
-            return super.onOptionsItemSelected(item);
-    }
-}
-public void openNewContent(){
-	Intent neweditTask = new Intent(this,NewEditTaskActivity.class);
-	startActivity(neweditTask);
-}
-public void openSettings(){
-	Intent setting = new Intent(this,Setting.class);
-	startActivity(setting);
+		
+		if(position==2){
+			Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
+			openAbout();
+
+		}
+		/*switch (position) {
+		case 0:
+			// openStat();
+
+		case 1:
+			// openSettings();
+
+		case 2:
+			Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
+			openAbout();
+			break;
+		default:
+			// return //
+		}*/
+	}
+
+	public void openAbout() {
+		
+		Intent about = new Intent(this, About.class);
+		startActivity(about);
+	}
+
 }
